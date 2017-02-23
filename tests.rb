849ad24ea279725ea30b320966a31d986bd94c76
Laura Moore <rileyle@gmail.com>
2017-02-22 21:02:28 -0500

    Broke some stuff, oops

--- tests.rb
+++ tests.rb
@@ -33,19 +33,19 @@ class FirstAppTest < Test::Unit::TestCase
   def test_tina_lipsum
     get "/lorem/tina"
     assert last_response.ok?
-    assert_equal Tina.text, last_response.body
+    assert_equal Tina.text, JSON.parse(last_response.body)['paragraphs']
   end
 
   def test_adev_lipsum
     get "/lorem/adev/4"
     assert last_response.ok?
-    assert_equal 4, last_response.body.split("</p>").count
+    assert_equal 4, JSON.parse(last_response.body)['paragraphs'].split("</p>").count
   end
 
   def test_futurama_lipsum
     get "/lorem/futurama/2"
     assert last_response.ok?
-    assert_equal 2, last_response.body.split("</p>").count
+    assert_equal 2, JSON.parse(last_response.body)['paragraphs'].split("</p>").count
   end
 
   def test_make_a_post
